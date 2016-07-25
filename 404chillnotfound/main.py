69b93e22fded5ecadd3b05640456bc896bb27fad
Savannah Bell <savannahbell9@gmail.com>
2016-07-25 16:26:28 -0400

    Added CollegeInfo model for datastore

--- 404chillnotfound/main.py
+++ 404chillnotfound/main.py
@@ -2,17 +2,21 @@
 
 import webapp2, jinja2, json
 from google.appengine.ext import ndb
-from pprint import pprint
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 with open('applications/institutionsapi.json') as data_file:
     collegeData = json.load(data_file)
 
-class CollegeEntry(ndb.Model):
+class CollegeInfo(ndb.Model):
     collegeName = ndb.StringProperty(required=True)
-    collegeCity = ndb.StringProperty(required=True)
-    collegeState = ndb.StringProperty(required=True)
-    collegeCountry = ndb.StringProperty(required=True)
+    collegeAddress = ndb.StringProperty(required=False)
+    collegeCity = ndb.StringProperty(required=False)
+    collegeState = ndb.StringProperty(required=False)
+    collegeZipCode = ndb.StringProperty(required=False)
+    collegeWebsite = ndb.StringProperty(required=False)
+    collegeLatitude = ndb.StringProperty(required=False)
+    collegeLongitude = ndb.StringProperty(required=False)
+    collegeAlias = ndb.StringProperty(required=False)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
