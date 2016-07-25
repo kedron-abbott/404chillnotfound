eab70aa0fe20b1a98089da83101f573b4fd47710
Savannah Bell <savannahbell9@gmail.com>
2016-07-25 11:19:16 -0400

    Added separate file for entry content

--- 404chillnotfound/main.py
+++ 404chillnotfound/main.py
@@ -7,8 +7,12 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        entryTemplate =  env.get_template('college-input.html')
+        entryContent = entryTemplate.render()
         indexTemplate = env.get_template('index.html')
-        self.response.out.write(indexTemplate.render({}))
+        self.response.out.write(indexTemplate.render({
+        'content':entryContent
+        }))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
