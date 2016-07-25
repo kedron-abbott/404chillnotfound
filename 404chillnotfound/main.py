da098a98789c6def87c7271a83577491ef94eec9
Savannah Bell <savannahbell9@gmail.com>
2016-07-25 15:21:10 -0400

    Made the colleges API!

--- 404chillnotfound/main.py
+++ 404chillnotfound/main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
-import webapp2, jinja2
+import webapp2, jinja2, json
 from google.appengine.ext import ndb
+from pprint import pprint
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+with open('applications/institutionsapi.json') as data_file:
+    collegeData = json.load(data_file)
+
+class CollegeEntry(ndb.Model):
+    collegeName = ndb.StringProperty(required=True)
+    collegeCity = ndb.StringProperty(required=True)
+    collegeState = ndb.StringProperty(required=True)
+    collegeCountry = ndb.StringProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
