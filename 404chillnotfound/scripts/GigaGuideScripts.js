7bbc57ef7e651c88e854ae180393748243434aa1
Savannah Bell <savannahbell9@gmail.com>
2016-07-26 12:17:38 -0400

    Added more comments

--- 404chillnotfound/scripts/GigaGuideScripts.js
+++ 404chillnotfound/scripts/GigaGuideScripts.js
@@ -1,10 +1,10 @@
 $("img.a").hover(
 	function() {
-    $( this ).stop().animate({"opacity": "0"}, "slow"); // this makes the black and white image look like it disapears. (i know i spelled disappears wrong, but i dont care to change it rn)
+    $( this ).stop().animate({"opacity": "0"}, "slow"); // this makes the black and white image look like it disappears
     console.log("fading out: ");
 	},
 	function() {
-    if (!$(this).data("clicked")) { //this part checks to see if the value of clicked is false (basically the either never clicked or they unclicked) and if the value is that way, it makes the black and white fade back in.
+    if (!$(this).data("clicked")) { // Checks to see if the value of clicked is false (basically they either never clicked or they unclicked) and if the value is that way, it makes the black and white fade back in
 		    $( this ).stop().animate({"opacity": "1"}, "slow");
       	console.log("fading in: ");
      }
@@ -12,33 +12,12 @@ $("img.a").hover(
 );
 
 $('img.a').on("click", sayhi);
-function sayhi () {$( this ).data("clicked",!$(this).data("clicked"));} //this checks to see if the value of clicked is true. If the value is true, and the image is clicked, it sets to false. If it's false and the image is clicked the value changes to true
+function sayhi () {$( this ).data("clicked",!$(this).data("clicked"));} // Checks to see if the value of clicked is true; if the value is true, and the image is clicked, it sets to false; if it's false and the image is clicked, the value changes to true
 
 
-
-
-
-
-//Below is Savannah's code -- this is the autocomplete feature on the site
-
-//$( function() {
-//    $( "#colleges" ).autocomplete({
-//      source: jsCollegeList
-//    });
-//  } );
-/*
-var options = {
-	data: jsCollegeList
-	placeholder: "College Name"
-	getValue: "name",
-	list: {
-		match: {
-			enabled: true
-		},
-		maxNumberOfElements: 8
-	},
-	theme: "plate-dark"
-};
-
-$("#plate").easyAutocomplete(options);
-*/
+// Autocomplete college name as user types
+$( function() {
+    $( "#colleges" ).autocomplete({
+      source: jsCollegeList
+    });
+} );
